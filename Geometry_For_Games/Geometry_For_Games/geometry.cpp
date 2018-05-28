0415ccd27a60eb56b0e83fd7bbde8385e95a5db5
Henry <henry983615@gmail.com>
2018-05-28 14:21:53 +1200

    You can now add two vectors

--- Geometry_For_Games/Geometry_For_Games/geometry.cpp
+++ Geometry_For_Games/Geometry_For_Games/geometry.cpp
@@ -5,6 +5,10 @@
 
 using namespace std;
 
+TVector3 AG;
+TVector3 BG;
+TVector3 CG;
+TVector3 DG;
 
 /*
 name of function: Equals
@@ -63,37 +67,36 @@ TVector3& Add(const TVector3& _krA, const TVector3& _krB, TVector3& _rResultant)
 	
 	system("cls");
 	float iT = 0;
-	/*
+
 	cout << "-= Vector 1 =-" << endl;
 	cout << "X: ";
 	cin >> iT;
-	_krA.m_fX = getfloat(iT);
+	AG.m_fX = getfloat(iT);
 
 	cout << "Y: ";
 	cin >> iT;
-	_krA.m_fY = getfloat(iT);
+	AG.m_fY = getfloat(iT);
 
 	cout << "Z: ";
 	cin >> iT;
-	_krA.m_fZ = getfloat(iT);
-
+	AG.m_fZ = getfloat(iT);
+	
 	cout << "-= Vector 2 =-" << endl;
 	cout << "X: ";
 	cin >> iT;
-	_krB.m_fX = getfloat(iT);
+	BG.m_fX = getfloat(iT);
 
 	cout << "Y: ";
 	cin >> iT;
-	_krB.m_fY = getfloat(iT);
+	BG.m_fY = getfloat(iT);
 
 	cout << "Z: ";
 	cin >> iT;
-	_krB.m_fZ = getfloat(iT);
-*/
+	BG.m_fZ = getfloat(iT);
 
-	_rResultant.m_fX = (_krB.m_fX + _krA.m_fX);
-	_rResultant.m_fY = (_krB.m_fY + _krA.m_fY);
-	_rResultant.m_fZ = (_krB.m_fZ + _krA.m_fZ);
+	_rResultant.m_fX = (AG.m_fX + BG.m_fX);
+	_rResultant.m_fY = (AG.m_fY + BG.m_fY);
+	_rResultant.m_fZ = (AG.m_fZ + BG.m_fZ);
 
 	cout << "Result: (" << _rResultant.m_fX << ", " << _rResultant.m_fY << ", " << _rResultant.m_fZ << ")" << endl;
 
