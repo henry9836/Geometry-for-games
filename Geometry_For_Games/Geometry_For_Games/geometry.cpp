2cd8bd99dace0ff16fcb8ad2db28fb1a1921352c
Henry <henry983615@gmail.com>
2018-05-28 14:40:28 +1200

    Subtracting Two Vectors Is Now Possible

--- Geometry_For_Games/Geometry_For_Games/geometry.cpp
+++ Geometry_For_Games/Geometry_For_Games/geometry.cpp
@@ -102,7 +102,47 @@ TVector3& Add(const TVector3& _krA, const TVector3& _krB, TVector3& _rResultant)
 
 	return _rResultant;
 }
-
+/*
+name of function : Subtract
+@author: Henry Oliver
+@parameter: Subtracts two vectors
+@return: result vector(struct)
+*/
 TVector3& Subtract(const TVector3& _krA, const TVector3& _krB, TVector3& _rResultant) {
+	system("cls");
+	float iT = 0;
 
+	cout << "-= Vector 1 (Original) =-" << endl;
+	cout << "X: ";
+	cin >> iT;
+	AG.m_fX = getfloat(iT);
+
+	cout << "Y: ";
+	cin >> iT;
+	AG.m_fY = getfloat(iT);
+
+	cout << "Z: ";
+	cin >> iT;
+	AG.m_fZ = getfloat(iT);
+
+	cout << "-= Vector 2 (To Subtract) =-" << endl;
+	cout << "X: ";
+	cin >> iT;
+	BG.m_fX = getfloat(iT);
+
+	cout << "Y: ";
+	cin >> iT;
+	BG.m_fY = getfloat(iT);
+
+	cout << "Z: ";
+	cin >> iT;
+	BG.m_fZ = getfloat(iT);
+
+	_rResultant.m_fX = (AG.m_fX - BG.m_fX);
+	_rResultant.m_fY = (AG.m_fY - BG.m_fY);
+	_rResultant.m_fZ = (AG.m_fZ - BG.m_fZ);
+
+	cout << "Result: (" << _rResultant.m_fX << ", " << _rResultant.m_fY << ", " << _rResultant.m_fZ << ")" << endl;
+
+	return _rResultant;
 }
